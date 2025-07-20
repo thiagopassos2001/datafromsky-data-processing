357c81df43ca1cb6eb8245dd17b3a28d136972ba
Thiago Passos <passos.thiago2001@gmail.com>
2025-07-20 06:36:05 -0300

    bug fix

--- model.py
+++ model.py
@@ -1,71 +1,11 @@
 import os
 import pandas as pd
 import warnings
+# import xlsxwriter
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=DeprecationWarning)
 
-def MatchVehicleType(vehicle_type):
-    """
-    Padronizar rótulos
-    """
-    valid_types = {
-        ' Undefined':"Indefinido",
-        ' Car':"Carro",
-        ' Medium Vehicle':"Carro",
-        ' Heavy Vehicle':"Caminhão",
-        ' Motorcycle':"Moto",
-        ' Light Truck':"Caminhão",
-        ' Bus':"Ônibus",
-        ' Van':"Carro",
-        ' Truck':"Caminhão",
-        ' Cartrailer':"Carro",
-        ' Tractor':"Caminhão",
-        ' Trucktrailer':"Carro",
-        ' Pedestrian':"Pedestre",
-        ' Bicycle':"Bicicleta",
-        ' Animal':"Animal",
-        ' Caravan':"Carro",
-        }
-    
-    if not vehicle_type in list(valid_types.keys()):
-        return None
-    
-    return valid_types[vehicle_type]
-
-def ConcatSequentialRecords(file_list):
-    """
-    Concatena dataframes e reseta o horário de cada sequencialmente
-    A sequência na ordem da lista
-    """
-    df_list = []
-    last_instant = 0
-    
-    for i in file_list:
-        df_ = pd.read_csv(i,sep=';',encoding='utf-8',encoding_errors="ignore")
-        df_.insert(0,"Arquivo",os.path.basename(i))
-        df_[" Track ID"] = df_["Arquivo"].astype(str) + "-" + df_[" Track ID"].astype(str)
-        df_[" Entry Time [s]"] = df_[" Entry Time [s]"] + last_instant
-        last_instant = df_[" Entry Time [s]"].max()
-        
-        df_list.append(df_)
-    
-    df = pd.concat(df_list,ignore_index=True).sort_values(" Entry Time [s]")
-    
-    return df
-
-def ValueUCP(vehicle_class):
-    UCP_class = {
-        "Carro":1,
-        "Moto":1/3,
-        "Caminhão":2,
-        "Ônibus":2,
-        }
-
-    if not vehicle_class in UCP_class.keys():
-        return 0
-    return UCP_class[vehicle_class]
-
-def AggOD(file_list,n_min=15,f_corr=None,f_corr_perc=1,vehicle_type_list=["Moto","Carro","Caminhão","Ônibus"]):
+def AggOD_old(file_list,n_min=15,f_corr=None,f_corr_perc=1,vehicle_type_list=["Moto","Carro","Caminhão","Ônibus"]):
     """
     Agrega os dados em uma matrix OD de n_min
     """
@@ -149,7 +89,7 @@ def AggOD(file_list,n_min=15,f_corr=None,f_corr_perc=1,vehicle_type_list=["Moto"
 
     return df_count
 
-def CountByRegion(file_list,f_corr_perc=1,vehicle_type_list=["Moto","Carro","Caminhão","Ônibus"]):
+def CountByRegion_old(file_list,f_corr_perc=1,vehicle_type_list=["Moto","Carro","Caminhão","Ônibus"]):
     # Le, contatena e compatibiliza os arquivos
     df = ConcatSequentialRecords(file_list)
     # Compatibilização dos tipos de veículos
@@ -170,17 +110,134 @@ def CountByRegion(file_list,f_corr_perc=1,vehicle_type_list=["Moto","Carro","Cam
     # Salvar
     df_count.to_excel(os.path.join(os.path.dirname(file_list[0]),f"CVC_Agg_por_Regiao_Concatenada.xlsx"))
 
-if __name__=="__main__":
-    file_list = [
-        r"C:\Users\thiagop\Desktop\Arquivos Locais\2025\72 - PSV\CE-065-1\DataFromSky\Rotatória\DJI_0980_comprimido_ffmpeg_mp4.csv",
-        r"C:\Users\thiagop\Desktop\Arquivos Locais\2025\72 - PSV\CE-065-1\DataFromSky\Rotatória\DJI_0982_comprimido_ffmpeg_mp4.csv",
-        r"C:\Users\thiagop\Desktop\Arquivos Locais\2025\72 - PSV\CE-065-1\DataFromSky\Rotatória\DJI_0984_comprimido_ffmpeg_mp4.csv",
-    ]
-    print(f"Processando arquivos... {len(file_list)}.")
+def MatchVehicleType(vehicle_type):
+    """
+    Padronizar rótulos
+    """
+    valid_types = {
+        ' Undefined':"Indefinido",
+        ' Car':"Carro",
+        ' Medium Vehicle':"Carro",
+        ' Heavy Vehicle':"Caminhão",
+        ' Motorcycle':"Moto",
+        ' Light Truck':"Caminhão",
+        ' Bus':"Ônibus",
+        ' Van':"Carro",
+        ' Truck':"Caminhão",
+        ' Cartrailer':"Carro",
+        ' Tractor':"Caminhão",
+        ' Trucktrailer':"Carro",
+        ' Pedestrian':"Pedestre",
+        ' Bicycle':"Bicicleta",
+        ' Animal':"Animal",
+        ' Caravan':"Carro",
+        }
     
-    CountByRegion(file_list,vehicle_type_list=["Moto","Carro","Caminhão","Ônibus",])
-    AggOD(file_list,n_min=10,vehicle_type_list=["Moto","Carro","Caminhão","Ônibus"])
+    if not vehicle_type in list(valid_types.keys()):
+        return None
+    
+    return valid_types[vehicle_type]
 
-    print(f"Arquivos processados {len(file_list)}.")
+def ConcatSequentialRecords(file_list):
+    """
+    Concatena dataframes e reseta o horário de cada sequencialmente
+    A sequência na ordem da lista
+    """
+    df_list = []
+    # first value
+    last_instant = 0
+    
+    for i in file_list:
+        df_ = pd.read_csv(i,sep=';',encoding='utf-8',encoding_errors="ignore")
+        df_.columns = [i.strip() for i in df_.columns]
+        df_.insert(0,"File",os.path.basename(i))
+        df_["Track ID"] = df_["File"].astype(str) + "_" + df_["Track ID"].astype(str)
+        df_["Entry Time [s]"] = df_["Entry Time [s]"] + last_instant
+        df_list.append(df_)
+
+        # update value
+        last_instant = df_["Entry Time [s]"].max()
+        
+    
+    df = pd.concat(df_list,ignore_index=True).sort_values("Entry Time [s]")
+    
+    
+    return df
+
+def ValueUCP(vehicle_class):
+    UCP_class = {
+        "Carro":1,
+        "Moto":1/3,
+        "Caminhão":2,
+        "Ônibus":2,
+        }
+
+    if not vehicle_class in UCP_class.keys():
+        return 0
+    return UCP_class[vehicle_class]
+
+def AggODByRegion(folder_path,drop_vehicle_types=[],file_extension="csv"):
+    os.chdir(folder_path)
+    file_list = os.listdir(folder_path)
+    file_list = [f for f  in file_list if f.endswith(f".{file_extension}")]
+
+    if len(file_list)==0:
+        print(f"A pasta '{folder_path}' não apresenta arquivos no formato '.{file_extension}'.")
+        return None
+
+    df = ConcatSequentialRecords(file_list)
+    df["Track Type"] = df["Track Type"].apply(MatchVehicleType)
+    df = df[-df["Track Type"].isin(drop_vehicle_types)]
+
+    df = df.sort_values(by="Entry Time [s]")
+
+    df = df.groupby(["Track ID"]).agg({
+        "Entry Time [s]":["first","last"],
+        "File":"first",
+        "Traffic Region ID":["first","last"],
+        "Track Type":"first"
+        }).reset_index(drop=False)
+    df.columns = df.columns.droplevel(1) + " " + df.columns.droplevel(0)
+
+    df["Weight"] = df["Track Type first"].apply(ValueUCP)
+
+    # df_count = pd.DataFrame()
+    # for i in df["Track Type"].unique():
+    #     df_count[i] = df.groupby("Traffic Region ID").apply(lambda x: x[x["Tipo de Veículo"]==i]["Tipo de Veículo"].count())
+
+    # Save excel
+    # Creating Excel Writer Object from Pandas  
+    with pd.ExcelWriter(
+        "DFS_processed.xlsx",
+        engine="xlsxwriter",
+        engine_kwargs={"options":{"strings_to_formulas":False,"strings_to_urls": False}}
+        ) as writer:
+
+        workbook = writer.book
+        worksheet = workbook.add_worksheet('Origin-destination pair')
+        writer.sheets['Origin-destination pair'] = worksheet
+        df.to_excel(writer,sheet_name='Origin-destination pair',startrow=0,startcol=0,index=False) 
+
+    return df
+
+if __name__=="__main__":
+    # folder_path = r"D:\Certare Engenharia (Local)\72 - Plano de Segurança Viária\CE-065-1\Simulação\Retorno FOR_FOR"
+    result = AggODByRegion(folder_path)
+    
+    
+    print(result)
+
+
+    # file_list = [
+    #     r"C:\Users\thiagop\Desktop\Arquivos Locais\2025\72 - PSV\CE-065-1\DataFromSky\Rotatória\DJI_0980_comprimido_ffmpeg_mp4.csv",
+    #     r"C:\Users\thiagop\Desktop\Arquivos Locais\2025\72 - PSV\CE-065-1\DataFromSky\Rotatória\DJI_0982_comprimido_ffmpeg_mp4.csv",
+    #     r"C:\Users\thiagop\Desktop\Arquivos Locais\2025\72 - PSV\CE-065-1\DataFromSky\Rotatória\DJI_0984_comprimido_ffmpeg_mp4.csv",
+    # ]
+    # print(f"Processando arquivos... {len(file_list)}.")
+    
+    # CountByRegion(file_list,vehicle_type_list=["Moto","Carro","Caminhão","Ônibus",])
+    # AggOD(file_list,n_min=10,vehicle_type_list=["Moto","Carro","Caminhão","Ônibus"])
+
+    # print(f"Arquivos processados {len(file_list)}.")
 
     # Comment test
